21abeae844dc616898b5eb9083d25b5571cef98e
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2021-08-13 14:49:07 +0530

    Update csv.py
    Csv

--- csv.py
+++ csv.py
@@ -13,7 +13,7 @@ if __name__=='__main__':
 while(condition):
     student_info=input("Enter student information for student #{} in the folloeing format (Name,Age,Contact_Numbetr,E-mail id):".format(student_num))
     student_info_list=student_info.split(' ')
-    print("\nThe entered information is- \nName: {}\nContact_number: {}\nE-mail id:{}".format(student_info_list[0],student_info_list[1],student_info_list[2],student_info_list[3]))
+    print("\nThe entered information is- \nName: {} \nAge: {} \nContact_number: {}\nE-mail id:{}".format(student_info_list[0],student_info_list[1],student_info_list[2],student_info_list[3]))
     choice_check=input("is entered information correct (yes/no):")
     if choice_check=="yes":
        write_into_csv(student_info_list)
